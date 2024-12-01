469e336cf3c5665a3c4dffbd984adce5881cda08
Павел Снетков <pavel@MacBook-Air-Pavel.local>
2024-12-01 20:21:21 +0500

    Fixed typescript issue

--- app/categories/[id]/page.tsx
+++ app/categories/[id]/page.tsx
@@ -2,7 +2,9 @@ import ProductList from '../../components/ProductList';
 import { apiRequest } from '../../components/utils/api';
 
 interface PageProps {
-  params: { id: string };
+    params: Promise<{
+        id: string;
+    }>;
 }
 
 interface Category {
@@ -12,19 +14,19 @@ interface Category {
 }
 
 export default async function CategoryPage({ params }: PageProps) {
-  const { id } = params;
-  
-  try {
-    const category = await apiRequest<Category>(`/api/v1/good-categories/${id}/`);
+    const { id } = await params; // Await the params to access id
     
-    return (
-      <div>
-        <h1 className="text-3xl font-bold mb-6">{category.name}</h1>
-        <ProductList categoryId={parseInt(id, 10)} />
-      </div>
-    );
-  } catch (error) {
-    console.error('Ошибка загрузки категории:', error);
-    return <div className="text-red-500">Ошибка загрузки категории</div>;
+    try {
+      const category = await apiRequest<Category>(`/api/v1/good-categories/${id}/`);
+      
+      return (
+        <div>
+          <h1 className="text-3xl font-bold mb-6">{category.name}</h1>
+          <ProductList categoryId={parseInt(id, 10)} />
+        </div>
+      );
+    } catch (error) {
+      console.error('Ошибка загрузки категории:', error);
+      return <div className="text-red-500">Ошибка загрузки категории</div>;
+    }
   }
-} 
